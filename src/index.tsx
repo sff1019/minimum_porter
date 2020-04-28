fd60a2339a098f7aeaae6c48be80d09340ea7230
sh8 <s@sh8.io>
2020-04-29 00:02:16 +0900

    Install a react-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 import './index.css';
-import store from './store';
+import { store, persistor } from './store';
 
 ReactDOM.render(
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>
     , document.getElementById('root'));
 
--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,28 @@
 import { combineReducers, createStore, compose, applyMiddleware} from 'redux'
 import { Reducer, State } from './reducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistPartial } from 'redux-persist/es/persistReducer'
+import storage from 'redux-persist/lib/storage'
 import thunk from "redux-thunk"
 
 export type AppState = {
-  state: State
+  state: State & PersistPartial
 }
 
-const storeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const storeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(
-    combineReducers<AppState>({
-      state: Reducer
-    }),
-    storeEnhancers(applyMiddleware(thunk))
-)
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default store
+const persistedReducer = persistReducer(persistConfig, Reducer)
+
+export const store = createStore(
+      combineReducers<AppState>({
+        state: persistedReducer,
+      }),
+      storeEnhancers(applyMiddleware(thunk))
+  )
+
+export const persistor = persistStore(store)
